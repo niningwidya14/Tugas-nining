d9b17228d82dda5024e09dcbf02a11f050722f25
Niningwidya <niningwidya304@gmail.com>
2017-03-12 18:08:34 +0700

    Tugas C++ nining

---
+++ Tugas Nining.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "hello world" << endl;
+cout << "i love living in this world" << endl;
+cout << " nama  : Nining Widya" << endl;
+cout << " kelas : TIF 16B " << endl;
+cout << " nim   : 16 111 230" << endl;
+return 0;
+}
